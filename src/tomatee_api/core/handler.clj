6623445279df3359e8c57b3fe628ff40c6a65af0
jyriand <jyriand@gmail.com>
2015-01-21 19:51:57 +0200

    Fixed main method

--- src/tomatee_api/core/handler.clj
+++ src/tomatee_api/core/handler.clj
@@ -1,11 +1,22 @@
 (ns tomatee-api.core.handler
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (pr-str ["Hello" :from 'Heroku])})
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(defroutes app
+  (GET "/" []
+       (splash))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
